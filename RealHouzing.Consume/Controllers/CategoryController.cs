575d6de9ac033881bb1cbd72f14b1e5529a9cec3
mert-akpinar <106198602+mert-akpinar@users.noreply.github.com>
2023-06-25 22:29:42 +0300

    Category Update, Delete,

--- RealHouzing.Consume/Controllers/CategoryController.cs
+++ RealHouzing.Consume/Controllers/CategoryController.cs
@@ -1,12 +1,84 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealHouzing.Consume.Models;
+using System.Net.WebSockets;
+using System.Text;
 
 namespace RealHouzing.Consume.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public CategoryController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44336/api/Category");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var JsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<CategoryListViewModel>>(JsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddCategory()
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> AddCategory(AddCategoryViewModel addCategoryViewModel)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(addCategoryViewModel);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:44336/api/Category", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:44336/api/Category/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateCategory(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:44336/api/Category/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var JsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateCategoryViewModel>(JsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryViewModel updateCategoryViewModel)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData= JsonConvert.SerializeObject(updateCategoryViewModel);
+            StringContent stringContent = new StringContent(jsonData,Encoding.UTF8,"application/json");
+            var responseMessage = await client.PutAsync("https://localhost:44336/api/Category/",stringContent);
+            if(responseMessage.IsSuccessStatusCode) 
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
     }
 }
